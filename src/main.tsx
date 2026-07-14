efe3e7e53d702e416951e2b11f0de565a5524e3f
LauraFmr <laurafmr90@gmail.com>
2026-07-14 20:07:25 +0200

    Added tailwind and shadcn (#2)
    * Added tailwind and shadcn
    
    * Remove baseUrl configuration from tsconfig.app.json
    
    * Update eslint file
    
    * Delete base url in tsconfig
    
    ---------
    
    Co-authored-by: Javi C <javicallejon88@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { Button } from './components/ui/button'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+<h1>Hola mundo</h1> 
+<Button>Enviar</Button> </StrictMode>,
 )
